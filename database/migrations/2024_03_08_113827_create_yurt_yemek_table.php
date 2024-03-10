607c62d7bb91d1640ce02508bd5282c95e48c6cd
bkaynar <kaynarburak@outlook.com>
2024-03-10 09:30:14 +0300

    yemek tablosu eklendi

--- database/migrations/2024_03_08_113827_create_yurt_yemek_table.php
+++ database/migrations/2024_03_08_113827_create_yurt_yemek_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,16 @@ return new class extends Migration
     {
         Schema::create('yurt_yemek', function (Blueprint $table) {
             $table->id();
+            $table->string('corba');
+            $table->string('anayemek');
+            $table->string('ara_yemek');
+            $table->string('diger_yiyecekler');
+            $table->string('tatlı')->nullable();
+            $table->string('meyve')->nullable();
+            $table->date('tarih');
+            $table->boolean('sabah-aksam');//0 sabah 1 aksam
+            $table->boolean('silindi_mi')->default('0');
+
             $table->timestamps();
         });
     }
